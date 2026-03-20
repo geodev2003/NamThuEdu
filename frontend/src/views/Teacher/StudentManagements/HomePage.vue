40028aebc1d6b3e790453f441a8096f9050d325a
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2026-03-20 14:18:35 +0700

    Remove frontend directory

--- frontend/src/views/Teacher/StudentManagements/HomePage.vue
+++
@@ -1,5 +0,0 @@
-
-
-<template>
-    <h1>Student Management Index Page</h1>
-</template>
--- frontend/src/views/Teacher/TestManagements/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <h1>Tests Managements</h1>
-</template>
-
-<script setup></script>
-
-<style scoped></style>
