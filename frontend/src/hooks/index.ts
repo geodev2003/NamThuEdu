863f3cf8480655385d172e6cd2f84c70cd3faae0
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2026-04-08 14:28:48 +0700

    s

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -3,6 +3,6 @@ export { useAgeGroup } from "./useAgeGroup";
 export { useAgeGroupSync } from "./useAgeGroupSync";
 export { useDebounce } from "./useDebounce";
 export { useLocalStorage } from "./useLocalStorage";
-export { usePageTitle } from "./usePageTitle";
+export { usePageTitle, PAGE_TITLES } from "./usePageTitle";
 export { useTeacherApi } from "./useTeacherApi";
 export { useTheme } from "./useTheme";
